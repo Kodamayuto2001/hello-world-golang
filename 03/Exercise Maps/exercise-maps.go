d7c90d3fc6e7ac9b9f590428783e10d681e329cd
kodamayuto <yuto2001gyokugan@gmail.com>
2021-05-02 12:19:17 +0900

    2021-0502-1219

--- 03/Exercise Maps/exercise-maps.go
+++ 03/Exercise Maps/exercise-maps.go
@@ -1,35 +1,58 @@
 package main
 
 import (
-	"fmt"
-
 	"golang.org/x/tour/wc"
 )
 
+// func WordCount(s string) map[string]int {
+// 	// if s[1] == ' ' {
+// 	// 	fmt.Println("ok")
+// 	// }
+// 	// fmt.Println(string(s[0:3]))
+// 	var a int = 0
+// 	var b int = 0
+// 	m := make(map[string]int)
+
+// 	for i := range s {
+// 		// fmt.Println(uint8(i))
+// 		if s[i] == ' ' {
+// 			if a == 0 {
+// 				m[string(s[0])] += 1
+// 				fmt.Println(string(s[0]))
+// 			} else {
+// 				m[string(s[a+1:b])] += 1
+// 				fmt.Println(s[a+1 : b])
+// 			}
+
+// 			a = int(i)
+// 		} else {
+// 			b = int(i + 1)
+// 		}
+// 	}
+// 	m[string(s[a+1:b])] += 1
+// 	fmt.Println(s[a+1 : b])
+
+// 	fmt.Println(m)
+
+// 	return m
+// }
+
 func WordCount(s string) map[string]int {
-	// if s[1] == ' ' {
-	// 	fmt.Println("ok")
-	// }
-	// fmt.Println(string(s[0:3]))
 	var a int = 0
 	var b int = 0
+	m := make(map[string]int)
 
 	for i := range s {
-		// fmt.Println(uint8(i))
 		if s[i] == ' ' {
-			if a == 0 {
-				fmt.Println(s[0])
-			} else {
-				fmt.Println(s[a+1 : b])
-			}
-
-			a = int(i)
-		} else {
-			b = int(i + 1)
+			b = i
+			// fmt.Println(string(s[a:b]))
+			m[string(s[a:b])] += 1
+			a = b + 1
 		}
 	}
-
-	return map[string]int{"x": 1}
+	// fmt.Println(string(s[a:]))
+	m[string(s[a:])] += 1
+	return m
 }
 
 func main() {
